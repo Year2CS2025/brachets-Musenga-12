8bd6e88c044c727bc450ca46c869d8fbd7fd7906
Musenga-12 <pmusenga567@gmail.com>
2026-05-08 10:14:44 +0200

    updated code

--- Balanced.java
+++ Balanced.java
@@ -1,13 +1,30 @@
+
+import java.util.HashMap;
 import java.util.Stack;
+
 public class Balanced{
     public static void main(String[] args) {
         String[] str = {"({[]})","({[})", "((()))", "({)}"};
         for(String s: str)
-         System.out.println(isBalanced(s));
+            System.out.println(isBalanced(s));
         
     }
     public static boolean isBalanced(String s){
-        //
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');    
+        map.put('[', ']');
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c: s.toCharArray()){
+            if(map.containsKey(c)){
+                stack.push(c);
+            }else if(map.containsValue(c)){
+                if(!stack.isEmpty() && map.get(stack.pop()) == c)
+                    return true;
+            }
+        }
         
         return false;
 }
